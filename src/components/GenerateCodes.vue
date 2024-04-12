01dc6dc72bc123e135dce911f477c71db93373e4
zhangJingJie <zhangsalute1@gmail.com>
2024-04-12 19:25:16 +0800

    完善-zhangjingjie

--- src/components/GenerateCodes.vue
+++ src/components/GenerateCodes.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h1> 生成激活码(管理员用) </h1>
+        <h1>生成激活码(管理员用)</h1>
         <a-form @submit.prevent="generateCodes">
             <a-form-item>
                 <span>生成数量</span>
@@ -11,15 +11,9 @@
                 <a-input-number v-model:value="length" :min="1" placeholder="激活码长度" style="width: 100%;" />
             </a-form-item>
             <a-form-item>
-                <span>到期日期</span>
-                <a-date-picker v-model:value="expiry" placeholder="到期日期" style="width: 100%;" />
-            </a-form-item>
-            <a-form-item>
                 <a-button html-type="submit">生成并提交至数据库</a-button>
-
-                <!-- {{ asd }} -->
                 <ul>
-                    <li v-for="(code, index) in asd" :key="index">{{ code }}</li>
+                    <li v-for="(code, index) in codes" :key="index">{{ code }}</li>
                 </ul>
             </a-form-item>
         </a-form>
@@ -29,25 +23,23 @@
 <script>
 import { ref } from 'vue';
 import axios from 'axios';
-import { message } from 'ant-design-vue'; // 引入message组件
+import { message } from 'ant-design-vue';
 
 export default {
     setup() {
         const count = ref(5);
         const length = ref(15);
-        const expiry = ref('');
-        let asd = ref('')
+        const codes = ref([]);
+
         const generateCodes = async () => {
             try {
                 const response = await axios.post('http://localhost:3000/generate-code', {
                     count: count.value,
                     length: length.value,
-                    expiry: expiry.value ? expiry.value.format('YYYY-MM-DD') : '',
                 });
 
-                // 打印生成的激活码
                 if (response.data.success) {
-                    asd.value = response.data.codes
+                    codes.value = response.data.codes;
                     console.log('生成的激活码:', response.data.codes);
                     message.success(response.data.message);
                 } else {
@@ -59,8 +51,7 @@ export default {
             }
         };
 
-
-        return { count, length, expiry, generateCodes, asd };
+        return { count, length, generateCodes, codes };
     },
 };
 </script>
